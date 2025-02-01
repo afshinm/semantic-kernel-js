affcedba7d0731474016bed1c5ff9afa778e87c8
Afshin Mehrabani <afshin.meh@gmail.com>
2025-02-01 09:35:22 +0000

    StreamingChatCompletionUpdate

--- src/abstractions/src/chatCompletion/ChatClient.ts
+++ src/abstractions/src/chatCompletion/ChatClient.ts
@@ -3,17 +3,15 @@ import { ChatClientMetadata } from './ChatClientMetadata';
 import { ChatCompletion } from './ChatCompletion';
 import { ChatMessage } from './ChatMessage';
 import { ChatOptions } from './ChatOptions';
+import { StreamingChatCompletionUpdate } from './StreamingChatCompletionUpdate';
 
 export abstract class ChatClient {
   abstract complete(chatMessages: ChatMessage[], options?: ChatOptions): Promise<ChatCompletion>;
 
-  /** 
-     * TODO
-    completeStreaming(
+  abstract completeStreaming(
         chatMessages: ChatMessage[],
         options?: ChatOptions,
     ): AsyncGenerator<StreamingChatCompletionUpdate>;
-    */
 
   abstract get metadata(): ChatClientMetadata;
   abstract getService<T>(serviceType: T, serviceKey?: string): object | undefined;
